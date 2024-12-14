cd95f61a64caf5578a0c871e7e3e7e4a08240ff3
e-johnson92 <johnson.evan92@gmail.com>
2024-12-13 22:25:37 -0500

    Final

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,19 +1,17 @@
-const username = document.getElementById('name').value;
-const age = document.getElementById('age').value;
-const email = document.getElementById('email').value;
-const job = document.getElementById('job').value;
-const designation = document.getElementById('designation').value;
-const productType = document.getElementById('productType').value;
-const feedback = document.getElementById('feedbackText').value;
-
 const submitButton = document.getElementById('submitBtn'); 
 
 submitButton.onclick = submitFeedback; 
 
 function submitFeedback()
 {
-    alert("Thank you for your valuable feedback."); 
-
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;
+    const userExperience = document.getElementById('userExperience').value; 
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
@@ -21,7 +19,7 @@ function submitFeedback()
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
-
+    document.getElementById('userExperienceFeedback').innerHTML = userExperience; 
     document.getElementById('userInfo').style.display = 'block'; 
 }
     
