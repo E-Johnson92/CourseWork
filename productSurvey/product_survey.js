c23aced0378aeb2ea8b3542ad360f8f43629c5fa
e-johnson92 <johnson.evan92@gmail.com>
2024-12-13 22:04:50 -0500

    still not working

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,3 +1,15 @@
+const username = document.getElementById('name').value;
+const age = document.getElementById('age').value;
+const email = document.getElementById('email').value;
+const job = document.getElementById('job').value;
+const designation = document.getElementById('designation').value;
+const productType = document.getElementById('productType').value;
+const feedback = document.getElementById('feedbackText').value;
+
+const submitButton = document.getElementById('submitBtn'); 
+
+submitButton.onclick = submitFeedback; 
+
 function submitFeedback()
 {
     alert("Thank you for your valuable feedback."); 
@@ -13,19 +25,6 @@ function submitFeedback()
     document.getElementById('userInfo').style.display = 'block'; 
 }
     
-    const username = document.getElementById('name').value;
-    const age = document.getElementById('age').value;
-    const email = document.getElementById('email').value;
-    const job = document.getElementById('job').value;
-    const designation = document.getElementById('designation').value;
-    const productType = document.getElementById('productType').value;
-    const feedback = document.getElementById('feedbackText').value;
-
-    const submitButton = document.getElementById('submitBtn'); 
-
-    submitButton.onclick = submitFeedback; 
-
-
 document.addEventListener('keydown', function(event)
 {
     if (event.key === 'Enter')
